1bb98f87df93b3f64384e7b8cf1567c9e34fc139
jjfuz <j.ross@uniandes.edu.co>
2025-11-24 20:05:24 -0500

    JR: Dependency tweak to deploy v2

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"], # CAMBIAR CUANDO EL FRONTEND ESTÉ EN PRODUCCIÓN
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
